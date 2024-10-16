96d4289635b8d7bf136b76bd5a137e3901e36dd9
MOHD MAUSHEEN <97398765+mausheen@users.noreply.github.com>
2024-10-16 10:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada',
+			   'Pierce the Veil', 'Norma Jean', 'The Bled',
+			   'Say Anything', 'The Midway State',
+			   'We Came as Romans', 'Counterparts',
+			   'Oh, Sleeper', 'A Skylit Drive',
+			   'Anywhere But Here','An Old Dog'];
+
+
+function removeEle(ele){
+	return ele.replace(/^(a |an | The)/i,'').trim();
+}
+
+const srtBonds=bonds.sort((a,b)=>removeEle(a).localeCompare(removeEle(b)));
+
+const bandList=document.getElementById("band");
+
+srtBonds.Foreach(ele=>{
+	const li=document.createElement("li");
+	li.textContent=ele;
+	bandList.appendChild(li);
+})
+
